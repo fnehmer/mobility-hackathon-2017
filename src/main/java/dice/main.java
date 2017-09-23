b172ad08d7896816ad191470677dc56f317e1be2
vngu <vinh-ngu@hotmail.com>
2017-09-23 05:28:40 +0200

    Floki.

--- src/main/java/dice/main.java
+++ src/main/java/dice/main.java
@@ -8,10 +8,9 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.digest.HmacUtils;
 import org.json.JSONObject;
 
-import java.util.Base64;
+import android.util.Base64;
 
 import okhttp3.Headers;
 import okhttp3.MediaType;
@@ -21,11 +20,14 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.internal.http2.Header;
 
+import javax.xml.bind.DatatypeConverter;
+
 public class main {
 
 	public static void main(String[] args) {
 		MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 		String geofox_url = "http://api-hack.geofox.de/gti/public/init";
+		// String geofox_url = "http://httpbin.org/anything";
 		OkHttpClient client = new OkHttpClient();
 
 		// the name
@@ -36,27 +38,50 @@ public class main {
 		// request json
 		JSONObject requestJson = new JSONObject();
 		requestJson.put("coordinateType", "EPSG_4326");
-		requestJson.put("maxList", 1);
-		requestJson.put("theName", theName);
+		requestJson.put("maxList", 2);
+		requestJson.put("theName", theName.toString());
 
 		RequestBody body = RequestBody.create(JSON, requestJson.toString());
-		String userSignature = HmacUtils.hmacSha1Hex("H4m$urgH13okt", requestJson.toString());
+
+		// passwort hash
+		String userSignature = mkSignature("H4m$urgH13okt", requestJson.toString().getBytes());
 		System.out.println(userSignature);
-		byte[] userSignature_byte = HmacUtils.hmacSha1("H4m$urgH13okt", requestJson.toString());
-		System.out.println(new String(userSignature_byte, 0));
 
 		Request request = new Request.Builder().url(geofox_url).post(body).addHeader("Accept", "application/json")
-				.addHeader("Content-Type", "application/json").addHeader("geofox-auth-user", "moby-hack")
-				.addHeader("geofox-auth-signature", userSignature_byte).build();
+				// .addHeader("Accept-Encoding", "gzip, deflate")
+				// .addHeader("Accept-Language", "en-US,en;q=0.8,de;q=0.6")
+				.addHeader("geofox-auth-type", "HmacSha1").addHeader("Content-Type", "application/json")
+				.addHeader("Geofox-Auth-User", "mobi-hack").addHeader("geofox-auth-signature", userSignature).build();
 		Response response;
 		try {
 			response = client.newCall(request).execute();
+			System.out.println(new JSONObject(response.body().string()));
 			System.out.println(response.body().string());
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	public static String mkSignature(String password, byte[] requestBody) {
+		final Charset passwordEncoding = Charset.forName("UTF-8");
+		final String algorithm = "HmacSHA1";
+
+		byte[] key = password.getBytes(passwordEncoding);
+		SecretKeySpec keySpec = new SecretKeySpec(key, algorithm);
+		Mac mac = null;
+		try {
+			mac = Mac.getInstance(algorithm);
+			mac.init(keySpec);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		}
+		byte[] signature = mac.doFinal(requestBody);
+
+		return DatatypeConverter.printBase64Binary(signature);
 	}
 
 }
