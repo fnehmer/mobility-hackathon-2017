efd73dd611263337342da72aef7e6dfc10130f8a
vngu <vinh-ngu@hotmail.com>
2017-09-23 09:18:19 +0200

    Clean up.

--- post.sh
+++
@@ -1,3 +0,0 @@
-curl -k -d "grant_type=client_credentials" \
--H "Authorization: Basic Y1pOR0JndTZMOGJhSjBxZnpfOW9mZGNxS3IwYTo2dHVtYkdZMEU5STJURGVveG1DUzRmS0hSVm9h" \
-https://api.deutschebahn.com/flinkster-api-ng/v1/areas
